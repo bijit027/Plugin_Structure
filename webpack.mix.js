20d61ac2f7884fefc6bf6f8de63e52511487e09d
bijit027 <debbijit70@gmail.com>
2022-06-30 11:45:40 +0600

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 let mix = require("laravel-mix");
 
 mix.setPublicPath("Assets");
-mix.setResourceRoot("../../wp-content/plugins/plugin_structure/Assets");
+mix.setResourceRoot("../../wp-content/plugins/Plugin_Structure/Assets");
 mix
   .js("src/Main.js", "Assets/Admin/Admin.js")
   .vue({ version: 3 })
